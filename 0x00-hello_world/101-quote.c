86e5c4ade7f305e803aee287f8894be7e577dc37
LEKARAM <skankaleks93@gmail.com>
2022-10-14 18:31:56 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,20 +5,20 @@
 
 
 /**
- *
  * *main - Entry point
  *
  * *Return: Always 1
  *
- * */
+ */
 
 
 
 int main(void)
 
 {
+	/*prints words followed by new line to standard errror
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
