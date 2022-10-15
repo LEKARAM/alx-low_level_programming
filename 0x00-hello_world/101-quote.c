22f3bc614ec8388d8681b7adcea932f51fdf4b48
LEKARAM <skankaleks93@gmail.com>
2022-10-14 19:35:27 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -16,7 +16,8 @@
 int main(void)
 
 {
-      /* prints a word using write function
+      /*
+       * prints a word using write function
        *
        */
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
