11652ccc35c33a0307004110f30d5da04e412f68
LEKARAM <skankaleks93@gmail.com>
2022-10-14 18:44:26 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -18,11 +18,12 @@ int main(void)
 {
 	/*prints words followed by new line to standard errror
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
-		59);
+		write(2, "and that piece of art is useful\" - Dora Korpar,
+	       	2015-10-19\n",	59);
 
 			return (1);
+			} 
+			
 
-}
 
 
