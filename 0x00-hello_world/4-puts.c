b8c418deb73e3c4bac99c5348fa8d927a81df6da
LEKARAM <skankaleks93@gmail.com>
2022-10-14 17:46:52 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,14 +6,14 @@
  *
  * *Return: Always 0 (Success)
  *
- *
  */
 
 
 
-int main(void)  //prints followed by a new line
+int main(void)
 
 {
+	//prints followed by a new line
 
 		puts("\"Programming is like building a multilingual puzzle");
 
