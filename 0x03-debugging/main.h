fc89d50fad8818f93c93825283406af186d62d7f
LEKARAM <skankaleks93@gmail.com>
2022-10-21 10:47:18 -0700

    task1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,4 @@
 #include <stdio.h>
 
 void positive_of_negative(int i);
-int largest_numbers(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-void convert_day(int month, int day);
-void print_times_table(int n);
-
 #endif
