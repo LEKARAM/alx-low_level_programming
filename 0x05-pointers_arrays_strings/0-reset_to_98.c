c936f3ff7b58b126f4fc2f0a72e11cac42473690
LEKARAM <skankaleks93@gmail.com>
2022-10-25 08:20:32 -0700

    all tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  takes a pointer to an int as par and updates value it points
+ * @n: int to check
+ * owned by sayanka
+ * Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
